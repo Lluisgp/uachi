a49bd6c23385a57a783d866dac26a1b2d202b4b4
lgonzalez <lgonzalez@opentrends.net>
2017-12-28 18:06:22 +0100

    minor changes

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,94 +2,78 @@
 
 class User extends CI_Controller {
 
-public function __construct(){
+    public function __construct() {
 
         parent::__construct();
-  			$this->load->helper('url');
-  	 		$this->load->model('user_model');
+        $this->load->helper('url');
+        $this->load->model('user_model');
         $this->load->library('session');
+    }
 
-}
+    public function index() {
+        $this->load->view("register.php");
+    }
 
-public function index()
-{
-$this->load->view("register.php");
-}
+    public function register_user() {
 
-public function register_user(){
-
-      $user=array(
-      'user_name'=>$this->input->post('user_name'),
-      'user_email'=>$this->input->post('user_email'),
-      'user_password'=>md5($this->input->post('user_password')),
-      'user_age'=>$this->input->post('user_age'),
-      'user_mobile'=>$this->input->post('user_mobile')
+        $user = array(
+            'user_name' => $this->input->post('user_name'),
+            'user_email' => $this->input->post('user_email'),
+            'user_password' => md5($this->input->post('user_password')),
+            'user_age' => $this->input->post('user_age'),
+            'user_mobile' => $this->input->post('user_mobile')
         );
         print_r($user);
 
-$email_check=$this->user_model->email_check($user['user_email']);
+        $email_check = $this->user_model->email_check($user['user_email']);
 
-if($email_check){
-  $this->user_model->register_user($user);
-  $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
-  redirect('user/login_view');
+        if ($email_check) {
+            $this->user_model->register_user($user);
+            $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
+            redirect('user/login_view');
+        } else {
 
-}
-else{
+            $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+            redirect('user');
+        }
+    }
 
-  $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
-  redirect('user');
+    public function login_view() {
 
+        $this->load->view("login.php");
+    }
 
-}
+    function login_user() {
+        $user_login = array(
+            'user_email' => $this->input->post('user_email'),
+            'user_password' => md5($this->input->post('user_password'))
+        );
 
-}
+        $data = $this->user_model->login_user($user_login['user_email'], $user_login['user_password']);
+        if ($data) {
+            $this->session->set_userdata('user_id', $data['user_id']);
+            $this->session->set_userdata('user_email', $data['user_email']);
+            $this->session->set_userdata('user_name', $data['user_name']);
+            $this->session->set_userdata('user_age', $data['user_age']);
+            $this->session->set_userdata('user_mobile', $data['user_mobile']);
 
-public function login_view(){
+            $this->load->view('user_profile.php');
+        } else {
+            $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+            $this->load->view("login.php");
+        }
+    }
 
-$this->load->view("login.php");
-
-}
-
-function login_user(){
-  $user_login=array(
-
-  'user_email'=>$this->input->post('user_email'),
-  'user_password'=>md5($this->input->post('user_password'))
-
-    );
-
-    $data=$this->user_model->login_user($user_login['user_email'],$user_login['user_password']);
-      if($data)
-      {
-        $this->session->set_userdata('user_id',$data['user_id']);
-        $this->session->set_userdata('user_email',$data['user_email']);
-        $this->session->set_userdata('user_name',$data['user_name']);
-        $this->session->set_userdata('user_age',$data['user_age']);
-        $this->session->set_userdata('user_mobile',$data['user_mobile']);
+    function user_profile() {
 
         $this->load->view('user_profile.php');
+    }
 
-      }
-      else{
-        $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
-        $this->load->view("login.php");
+    public function user_logout() {
 
-      }
-
-
-}
-
-function user_profile(){
-
-$this->load->view('user_profile.php');
-
-}
-public function user_logout(){
-
-  $this->session->sess_destroy();
-  redirect('user/login_view', 'refresh');
-}
+        $this->session->sess_destroy();
+        redirect('user/login_view', 'refresh');
+    }
 
 }
 
