574efca61742f1d55d8859fb41255b9d7de07166
Mad <Mad@lsgzWin7x64>
2018-01-29 19:12:25 +0100

    prova que no funciona

--- application/views/show.php
+++ application/views/show.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=0.6, user-scalable=0">
+        <meta property="og:url"           content="http://uachit.yuu.es/" />
+        <meta property="og:type"          content="website" />
+        <meta property="og:title"         content="Your Website Title" />
+        <meta property="og:description"   content="Your description" />
+        <meta property="og:image"         content="http://bosfundacja.pl/wp-content/uploads/2017/04/wchat-it.jpg" />
+ 
         <title>Uachit</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
@@ -34,10 +40,35 @@
                     $("#comment").focus();
                 })
             });
+            window.fbAsyncInit = function () {
+                FB.init({
+                    appId: '165122097437172',
+                    autoLogAppEvents: true,
+                    xfbml: true,
+                    version: 'v2.11'
+                });
+                FB.ui(
+                        {
+                            method: 'share',
+                            href: 'https://developers.facebook.com/docs/'
+                        }, function (response) {});
+            };
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
         </script>
     </head>
     <body>
-        <?php $this->view('header'); ?>         
+        <?php $this->view('header'); ?>  
+        <div id="fb-root"></div>
         <div class="container-fluid">
             <?php
             if ($data) {
@@ -77,12 +108,18 @@
                     <form id="comment_form" action="<?= base_url() ?>media/add_comment" method='post'>                  
                         <div class="form-group">  
                             <span id="lblresp" style="display:none;" class="badge">Respondiendo</span>
-                            <textarea autofocus class="form-control" name="comment_body" value="" placeholder="Comentario" id='comment'></textarea>
+                            <textarea class="form-control" name="comment_body" value="" placeholder="Comentario" id='comment'></textarea>
                         </div>
 
                         <input type='hidden' name='parent_id' value="0" id='parent_id' />
                         <input type='hidden' name='ne_id' value="<?= $data['media_id'] ?>" id='parent_id'/>
-
+                        <div data-layout="button_count" data-href="http://uachit.yuu.es/" onclick="FB.ui()" style="color:white; background-color: rgb(59, 89, 152); 
+                             width: 29px; line-height: 29px; height: 29px; 
+                             background-size: 16px; border-radius: 2px;" class="fb-send fb-root float-left" id="share-facebook"> 
+                            <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                            <path fill="#FFF" d="M17.78 27.5V17.008h3.522l.527-4.09h-4.05v-2.61c0-1.182.33-1.99 2.023-1.99h2.166V4.66c-.375-.05-1.66-.16-3.155-.16-3.123 0-5.26 1.905-5.26 5.405v3.016h-3.53v4.09h3.53V27.5h4.223z">
+                            </path></svg>   
+                        </div>
                         <div class="float-right" id='submit_button'>
                             <input class="btn btn-success" type="submit" name="submit" value="Añadir comentario"/>
                         </div>
